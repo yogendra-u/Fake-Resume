9a9beb25bcda149a99bdc9ec7ecf9da6804dc99d
Raghu Patgar <raghu.patgar2011@gmail.com>
2023-07-04 17:16:02 +0530

    first comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Index from './component/example';
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Index />
     </div>
   );
 }
---
+++ src/component/example/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+
+function Index () {
+    return(
+        <>
+        <h1>hi</h1>
+        </>
+    )
+}
+
+export default Index;
